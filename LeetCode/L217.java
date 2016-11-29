3e00c1e1e12dd33e12636117047078eb5de9b81c
vannahz <waner0120@gmail.com>
2016-11-29 21:16:56 +0800

    Create L217.java

---
+++ LeetCode/L217.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+		int len = nums.length;
+		for(int i=0; i<len; i++){
+			if(set.contains(nums[i]))
+				return true;
+			
+			set.add(nums[i]);
+		}
+		return false;
+    }
+}
