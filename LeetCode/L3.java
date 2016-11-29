e2061446e6bbb3d7b1a0e3c3d600fa0e33154840
vannahz <waner0120@gmail.com>
2016-11-29 21:17:36 +0800

    Create L3.java

---
+++ LeetCode/L3.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int ans = 0;
+	     int[] C = new int[256];  //represent character.
+	     int j=0;
+	     for(int i=0; i<s.length(); i++){
+	    	 while(j<s.length() && C[s.charAt(j)] == 0){
+	    		 C[s.charAt(j)] = 1;
+	    		 ans = Math.max(ans, j-i+1);
+	    		 j++;
+	    	 }
+	    	 C[s.charAt(i)] = 0;
+	     }
+		 return ans;
+    }
+}
